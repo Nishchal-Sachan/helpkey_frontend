31a3190426389026a6a6ca503c06be8b4e050e71
Nishchal-Sachan <sachannishchal@gmail.com>
2025-04-06 16:59:02 +0530

    configured auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,11 +2,11 @@ export async function checkAuth() {
     try {
       const res = await fetch("https://helpkey-backend.vercel.app/api/authuser", {
         method: "GET",
-        credentials: "include", // 👈 important: send cookies
+        credentials: "include", // send cookies
       });
   
       const data = await res.json();
-      return data.isAuthenticated ? data.user : null;
+      return data.success ? data.adminId : null; // ✅ updated here
     } catch (err) {
       console.error("Auth check failed:", err);
       return null;
